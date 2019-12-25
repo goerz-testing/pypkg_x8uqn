6e24fa2b81d5134c4dcb0dc8f563ac9e3e419504
Michael Goerz <goerz@stanford.edu>
2019-12-25 17:01:53 -0500

    Add a test for "Hello World"

--- tests/test_pypkg_x8uqn.py
+++ tests/test_pypkg_x8uqn.py
@@ -1,6 +1,8 @@
 """Tests for `pypkg_x8uqn` package."""
 
-import pytest
+import contextlib
+import io
+
 from pkg_resources import parse_version
 
 import pypkg_x8uqn
@@ -11,3 +13,11 @@ def test_valid_version():
     v_curr = parse_version(pypkg_x8uqn.__version__)
     v_orig = parse_version("0.1.0-dev")
     assert v_curr >= v_orig
+
+
+def test_hello_world():
+    """Test the hello_world function."""
+    stdout = io.StringIO()
+    with contextlib.redirect_stdout(stdout):
+        pypkg_x8uqn.mod.hello_world()
+    assert "Hello World" in stdout.getvalue()
