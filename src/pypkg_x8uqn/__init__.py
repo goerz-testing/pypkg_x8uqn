ef8677a37842ce04298b18100c0712d3f1d8bacf
Michael Goerz <goerz@stanford.edu>
2019-12-25 12:31:27 -0500

    Add code

--- src/pypkg_x8uqn/__init__.py
+++ src/pypkg_x8uqn/__init__.py
@@ -1,8 +1,14 @@
-"""Top-level package for PyPkg X8UQN."""
+"""Top-level package."""
 
 __version__ = '0.1.0-dev'
 
-# All members whose name does not start with an underscore must be listed
-# either in __all__ or in __private__
-__all__ = []
+__all__ = ['func', 'PROJECT_CONST']
 __private__ = []
+__known_refs__ = {'PROJECT_CONST': ':obj:`~.subpkg.submod.PROJECT_CONST`'}
+
+from .subpkg.submod import PROJECT_CONST
+
+
+def func(a):
+    """This is a function that just returns `a`."""
+    return a
