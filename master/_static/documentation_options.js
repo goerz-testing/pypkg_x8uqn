eead62f7421e84842fcdb93709fd2f3abc2b8ab1
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-12-27 05:20:14 +0000

    Update docs after building Travis build 143 of goerz-forks/pypkg_x8uqn
    The docs were built from the branch 'master' against the commit
    5cde4e2df558b5c924178714393010d0f9c7d23b.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/goerz-forks/pypkg_x8uqn/jobs/629889910.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/lib/python3.7/site-packages/doctr/__main__.py deploy --key-path docs/doctr_deploy_key.enc '--command=git show 5cde4e2df558b5c924178714393010d0f9c7d23b:.travis/doctr_post_process.py > post_process.py && git show 5cde4e2df558b5c924178714393010d0f9c7d23b:.travis/versions.py > versions.py && python post_process.py' --built-docs docs/_build/html --no-require-master --build-tags master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.2+dev (b5e5b3b)',
+    VERSION: '0.1.2+dev (5cde4e2)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
